071f98f1d63e186f647d0790bc3c8833ee76c9c3
aulandsdalen <aulandsdalen@gmail.com>
2016-10-16 18:53:59 +0300

    image upload prototype; image view added

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
 set :bind, '0.0.0.0'
 set :views, settings.root + '/views'
-
 get '/' do
 	haml :index
 end
@@ -9,3 +8,22 @@ end
 get '/upload' do 
 	haml :upload
 end	
+
+post '/upload' do
+	if params['up_image'][:type] != "image/png"
+		return "File type error... only image/png's are allowed"
+	end
+	filename = 'public/'+params['up_image'][:filename]
+	File.open('public/' + params['up_image'][:filename], 'w') do |f|
+		f.write(params['up_image'][:tempfile].read)
+	end
+	return filename
+end
+
+get '/image/:filename' do
+	if (File.exist?('public/' + params[:filename] + '.png'))
+		haml :image, :locals => {:image => '/' + params[:filename] + '.png'}
+	else
+		"No such image"
+	end
+end
