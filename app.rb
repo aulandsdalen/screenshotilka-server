1d4b867c9c97900db3e4fe94aa537a3d18c96198
aulandsdalen <aulandsdalen@gmail.com>
2016-10-16 18:08:27 +0300

    index & upload views added; fixes to app.rb

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
 set :bind, '0.0.0.0'
+set :views, settings.root + '/views'
 
-class Screenshotilka < Sinatra::Base
-	get '/' do
-		
-	end
+get '/' do
+	haml :index
 end
+
+get '/upload' do 
+	haml :upload
+end	
